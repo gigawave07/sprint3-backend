ee10715f656d06a90ac031ede9e415a82bde98cc
chauluong1995 <supea52795@gmail.com>
2021-01-22 09:38:34 +0700

    update send mail

--- src/main/java/com/sprint3/backend/services/CheckThesisService.java
+++ src/main/java/com/sprint3/backend/services/CheckThesisService.java
@@ -1,4 +1,9 @@
 package com.sprint3.backend.services;
 
+import java.util.List;
+
+import com.sprint3.backend.entity.CheckThesis;
+
 public interface CheckThesisService {
+    List<CheckThesis> findAll();
 }
--- src/main/java/com/sprint3/backend/services/impl/CheckThesisServiceImpl.java
+++ src/main/java/com/sprint3/backend/services/impl/CheckThesisServiceImpl.java
@@ -1,6 +1,22 @@
 package com.sprint3.backend.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import com.sprint3.backend.entity.CheckThesis;
+import com.sprint3.backend.repository.CheckThesisReository;
 import com.sprint3.backend.services.CheckThesisService;
 
+@Service
 public class CheckThesisServiceImpl implements CheckThesisService {
+
+    @Autowired
+    private CheckThesisReository checkThesisReository;
+
+    @Override
+    public List<CheckThesis> findAll() {
+        return this.checkThesisReository.findAll();
+    }
 }
