c87f81111f1b7de06238e41033988e08d6ce89cb
Your Name <khanh06111993@gmail.com>
2021-01-25 09:16:54 +0700

    backend-25/01/2021

--- src/main/java/com/sprint3/backend/controllers/TeacherController.java
+++ src/main/java/com/sprint3/backend/controllers/TeacherController.java
@@ -1,11 +1,41 @@
 package com.sprint3.backend.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sprint3.backend.entity.Teacher;
+import com.sprint3.backend.model.TeacherDTO;
+import com.sprint3.backend.services.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/teacher")
 @CrossOrigin
 public class TeacherController {
+    @Autowired
+    private TeacherService teacherService;
+
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Void> editUser(@PathVariable Long id, @RequestBody TeacherDTO teacherDTO) {
+        Teacher teacher = teacherService.findById(id);
+        if (teacher == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            teacher.setFullName(teacherDTO.getFullName().trim());
+            teacher.setTeacherCode(teacherDTO.getTeacherCode().trim());
+            teacher.setEmail(teacherDTO.getEmail().trim());
+            teacher.setPhone(teacherDTO.getPhone().trim());
+            teacherService.save(teacher);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Teacher> deleteEmployeeById(@PathVariable Long id) {
+        Teacher teacher = this.teacherService.findById(id);
+        teacher.getAppAccount().setAppRole(null);
+        this.teacherService.deleteByID(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
