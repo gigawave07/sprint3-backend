1d7a23117d49d51e719bf1ecb6c74f988ac58ae6
Nguyen Quoc Khanh <pkhanh2994@gmail.com>
2021-01-26 15:40:33 +0700

    26/1/2021 up  nhan ngay danh no vang 2

--- src/main/java/com/sprint3/backend/repository/AppAccountRepository.java
+++ src/main/java/com/sprint3/backend/repository/AppAccountRepository.java
@@ -1,9 +1,9 @@
 package com.sprint3.backend.repository;
 
-import com.sprint3.backend.entity.AppAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+        import com.sprint3.backend.entity.AppAccount;
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.data.jpa.repository.Query;
+        import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppAccountRepository extends JpaRepository<AppAccount, Long> {
