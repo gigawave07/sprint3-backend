dced6d158a9135ee256d48166f523db571d66133
Nguyen Quoc Khanh <pkhanh2994@gmail.com>
2021-01-26 15:29:46 +0700

    26/1/2021 up  nhan ngay danh no vang

--- src/main/java/com/sprint3/backend/repository/AppAccountRepository.java
+++ src/main/java/com/sprint3/backend/repository/AppAccountRepository.java
@@ -2,8 +2,11 @@ package com.sprint3.backend.repository;
 
 import com.sprint3.backend.entity.AppAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppAccountRepository extends JpaRepository<AppAccount, Long> {
+    @Query(value = "select * from app_account where username =?", nativeQuery = true)
+    AppAccount findAccountByUserName(String username);
 }
--- src/main/java/com/sprint3/backend/repository/InstructionRepository.java
+++ src/main/java/com/sprint3/backend/repository/InstructionRepository.java
@@ -2,6 +2,7 @@ package com.sprint3.backend.repository;
 
 import com.sprint3.backend.entity.Instruction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
