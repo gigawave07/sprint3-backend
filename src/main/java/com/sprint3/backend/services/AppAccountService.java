40d916aedd935786dd5ebd5bc2401ef468ea52e2
Nguyen Quoc Khanh <pkhanh2994@gmail.com>
2021-01-27 09:39:12 +0700

    27/1/2021 up  nhan ngay danh no di hoc 1

--- src/main/java/com/sprint3/backend/services/AppAccountService.java
+++ src/main/java/com/sprint3/backend/services/AppAccountService.java
@@ -1,7 +1,10 @@
 package com.sprint3.backend.services;
 
 import com.sprint3.backend.entity.AppAccount;
+import org.springframework.stereotype.Service;
 
 public interface AppAccountService {
     AppAccount findAccountByUserName(String username);
+    AppAccount findAccountById(Long id);
+    void changePasswordUser(String password, Long id);
 }
--- src/main/java/com/sprint3/backend/services/AppAdminService.java
+++ src/main/java/com/sprint3/backend/services/AppAdminService.java
@@ -1,4 +1,5 @@
 package com.sprint3.backend.services;
 
 public interface AppAdminService {
+    void updateAdminInfo(String fullName, String email, String address, Long id);
 }
