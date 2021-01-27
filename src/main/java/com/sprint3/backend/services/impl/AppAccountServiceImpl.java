7184520ec976bab86e1ddc9ef94e6fdd0ca653c1
Nguyen Quoc Khanh <pkhanh2994@gmail.com>
2021-01-27 09:39:12 +0700

    27/1/2021 up  nhan ngay danh no di hoc 1

--- src/main/java/com/sprint3/backend/services/impl/AppAccountServiceImpl.java
+++ src/main/java/com/sprint3/backend/services/impl/AppAccountServiceImpl.java
@@ -4,12 +4,24 @@ import com.sprint3.backend.entity.AppAccount;
 import com.sprint3.backend.repository.AppAccountRepository;
 import com.sprint3.backend.services.AppAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AppAccountServiceImpl implements AppAccountService {
     @Autowired
     private AppAccountRepository appAccountRepository;
     @Override
     public AppAccount findAccountByUserName(String username) {
         return this.appAccountRepository.findAccountByUserName(username);
+    }
+
+    @Override
+    public AppAccount findAccountById(Long id) {
+        return this.appAccountRepository.findAccountById(id);
+    }
+
+    @Override
+    public void changePasswordUser(String password, Long id) {
+        this.appAccountRepository.changePasswordUser(password,id);
     }
 }
