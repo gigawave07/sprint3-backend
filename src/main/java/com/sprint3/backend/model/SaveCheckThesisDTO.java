837423936779bdd5a2be10b7db7d5c994dc5eca8
Nguyen Quang Danh <you@example.com>
2021-01-28 14:21:24 +0700

    danh_nq 28/1 "save check thesis"

--- src/main/java/com/sprint3/backend/model/SaveCheckThesisDTO.java
+++ src/main/java/com/sprint3/backend/model/SaveCheckThesisDTO.java
@@ -13,4 +13,7 @@ public class SaveCheckThesisDTO {
     Boolean status;
     String checkDate;
     String idCheckThesis;
+
+    String studentGroupId;
+    String thesisId;
 }
--- src/main/java/com/sprint3/backend/services/impl/CheckThesisServiceImpl.java
+++ src/main/java/com/sprint3/backend/services/impl/CheckThesisServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sprint3.backend.services.impl;
 
+import com.sprint3.backend.entity.CheckThesis;
 import com.sprint3.backend.model.CheckThesisDTO;
 import com.sprint3.backend.repository.CheckThesisRepository;
 import com.sprint3.backend.services.CheckThesisService;
@@ -15,18 +16,41 @@ public class CheckThesisServiceImpl implements CheckThesisService {
     @Autowired
     private CheckThesisRepository checkThesisRepository;
 
-    /*List check thesis*/
+    /*List check thesis unapproved*/
     @Override
     public List<CheckThesisDTO> getAllCheckThesis(Long idTeacherParam) {
         return this.checkThesisRepository.getAllCheckThesis(idTeacherParam);
     }
 
+    /*List check thesis approved*/
+    @Override
+    public List<CheckThesisDTO> getAllCheckThesisApproved(Long idTeacher) {
+        return this.checkThesisRepository.getAllCheckThesisApproved(idTeacher);
+    }
+
+
     /*Save check thesis*/
     @Override
     public void saveCheckThesis(LocalDateTime checkDate, Long idCheckThesis) {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d HH:mm:ss");
-//        LocalDateTime localFromDay = LocalDateTime.parse(checkDate, formatter);
         this.checkThesisRepository.saveCheckThesis(checkDate, idCheckThesis);
+    }
+
+    /*Display list delete thesis unapproved*/
+    @Override
+    public List<CheckThesis> loadListDeleteCheckThesis(Long idTeacherParam) {
+        return this.checkThesisRepository.loadListDeleteCheckThesis(idTeacherParam);
+    }
+
+    /*Update check thesis*/
+    @Override
+    public void saveUpdateCheckThesis(Long idParam) {
+        this.checkThesisRepository.saveUpdateCheckThesis(idParam);
+    }
+
+    /*Delete list check thesis unapproved*/
+    @Override
+    public void deleteCheckThesisUnApproved(Long idCheckThesis) {
+        this.checkThesisRepository.deleteById(idCheckThesis);
     }
 
 }
