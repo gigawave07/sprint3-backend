79346f1d43ec2d0b4c0fdf7d5aa1bc212539d171
Nguyen Quoc Khanh <pkhanh2994@gmail.com>
2021-01-31 22:06:20 +0700

    31/1/2021 up new

--- src/main/java/com/sprint3/backend/repository/StudentRepository.java
+++ src/main/java/com/sprint3/backend/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.sprint3.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sprint3.backend.entity.Student;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
