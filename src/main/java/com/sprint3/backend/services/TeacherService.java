5d82a458bdd4be79beb04310921850bda0f3e4c4
nhatlanh2202 <nhatlanh2202@gmail.com>
2021-02-01 14:18:23 +0700

    lanh update 14h20 01/02/2021

--- src/main/java/com/sprint3/backend/services/TeacherService.java
+++ src/main/java/com/sprint3/backend/services/TeacherService.java
@@ -1,4 +1,16 @@
 package com.sprint3.backend.services;
 
+import com.sprint3.backend.entity.Teacher;
+
 public interface TeacherService {
+    /**
+     * Lành start
+     * @param id
+     * @return teacher
+     */
+    Teacher findByTeacherId(Long id);
+
+    /**
+     * lành end
+     */
 }
--- src/main/java/com/sprint3/backend/services/impl/TeacherServiceImpl.java
+++ src/main/java/com/sprint3/backend/services/impl/TeacherServiceImpl.java
@@ -1,6 +1,28 @@
 package com.sprint3.backend.services.impl;
 
+import com.sprint3.backend.entity.Teacher;
+import com.sprint3.backend.repository.TeacherRepository;
 import com.sprint3.backend.services.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TeacherServiceImpl implements TeacherService {
+
+    /**
+     * Lành start
+     * 
+     */
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
+    @Override
+    public Teacher findByTeacherId(Long id) {
+        return teacherRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * Lành end
+     */
 }
