537f4f204a55fbc88e8e322a4b33180d1c8bd697
luongvuquangdin <luongvuquangdin@gmail.com>
2021-02-01 08:53:34 +0700

    update new

--- src/main/java/com/sprint3/backend/controllers/InteractionController.java
+++ src/main/java/com/sprint3/backend/controllers/InteractionController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @RequestMapping("/interaction")
 @CrossOrigin
 public class InteractionController {
-    // Din
+    // Din start
     @Autowired
     InteractionService interactionService;
     @PostMapping("/create/{content}/{title}/{idAccount}")
--- src/main/java/com/sprint3/backend/controllers/NotificationController.java
+++ src/main/java/com/sprint3/backend/controllers/NotificationController.java
@@ -1,11 +1,23 @@
 package com.sprint3.backend.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sprint3.backend.services.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notification")
 @CrossOrigin
 public class NotificationController {
+    // Din start
+    @Autowired
+    NotificationService notificationService;
+    @PostMapping("/create/{idInteraction}/{idAccount}")
+    public ResponseEntity<Void> createInteraction(@PathVariable Long idInteraction, @PathVariable Long idAccount){
+
+        this.notificationService.createNotification(idInteraction, idAccount);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    // Din end
 }
